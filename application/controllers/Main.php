ffc1a4aecef615a86ce09446173fdaf1b603a1e6
HoJunLee <jazzwave14@gmail.com>
2017-12-29 14:34:25 +0900

    dldldl

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -5,9 +5,47 @@ class Main extends CI_Controller{
     {
         parent::__construct();
      //   $this->account_model = edu_get_instance('account_model', 'model');
-        $this->load->helper(array('form', 'url')); 
+        //    $this->load->helper(array('form', 'url')); 
     }
-    
+
+
+    public function index()
+    {
+        $data = array();  
+        $this->load->view('main/index', $data);
+        
+        // id check
+        //if(! $account_id = $this->_isLogin() )
+        //{
+        //    alert('로그인 후 이용하세요.','/login');
+        //    die;
+        //}
+
+        // get account info
+        //$acc  = edu_get_instance('AccountClass');
+        //$oAcc = new $acc($account_id);
+
+        // get note info
+        //$usn = $oAcc->oAccInfo->usn;
+        //$note  = edu_get_instance('NoteClass');
+        //$oNote = new $note($usn);
+
+        //$data = array();
+        //$aUserInfo = array(
+        //        'oAccountInfo'   => $oAcc->oAccInfo
+        //       ,'oNoteInfo'      => $oNote->oNoteInfo
+        //    );
+        //$data['aUserInfo'] = $aUserInfo;
+
+        //// test code
+        //echo "<!--";
+        //print_r($data);
+        //echo "-->";
+
+        //$this->load->view('main/dashboard', $data);
+    }
+
+
     public function test()
     {
         //echo "aaaa";
@@ -48,50 +86,18 @@ class Main extends CI_Controller{
     }
 
 
-    public function index()
-    {
-        
-        // id check
-        if(! $account_id = $this->_isLogin() )
-        {
-            alert('로그인 후 이용하세요.','/login');
-            die;
-        }
-
-        // get account info
-        $acc  = edu_get_instance('AccountClass');
-        $oAcc = new $acc($account_id);
-
-        // get note info
-        $usn = $oAcc->oAccInfo->usn;
-        $note  = edu_get_instance('NoteClass');
-        $oNote = new $note($usn);
-
-        $data = array();
-        $aUserInfo = array(
-                'oAccountInfo'   => $oAcc->oAccInfo
-               ,'oNoteInfo'      => $oNote->oNoteInfo
-            );
-        $data['aUserInfo'] = $aUserInfo;
-
-        // test code
-        echo "<!--";
-        print_r($data);
-        echo "-->";
-
-        $this->load->view('main/dashboard', $data);
-    }
 
 
     private function _isLogin()
     {
-        edu_get_instance('LoginClass');
-        $oMemberInfo = LoginClass::isLogin();
-        
-        if($oMemberInfo->usn)
-            return $oMemberInfo->account;
-
-        return false;
+        return true; 
+//      edu_get_instance('LoginClass');
+//      $oMemberInfo = LoginClass::isLogin();
+//      
+//      if($oMemberInfo->usn)
+//          return $oMemberInfo->account;
+//
+//      return false;
     }
 
 }
