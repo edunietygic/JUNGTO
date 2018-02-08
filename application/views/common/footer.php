2223a3357baf38bc8304385464ee4eb163590b17
ziyunn <jykim306@gmail.com>
2018-02-08 17:56:11 +0900

    view > common 생성 후 분리작업 진행 중

---
+++ application/views/common/footer.php
@@ -0,0 +1,10 @@
+  <!-- Footer -->
+  <footer id="footer" class="footer-light">
+    <div class="copyright-content">
+      <div class="container">
+        <div class="copyright-text text-center">&copy; 2017 hihappyschool -  All Rights Reserved.hihappyschool </div>
+      </div>
+    </div>
+  </footer>
+  <!-- end: Footer --> 
+ 
