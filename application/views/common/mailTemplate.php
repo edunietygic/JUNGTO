44034d2a5f3bbe91f9703b390da7e953f65d9cd5
ziyunn <jykim306@gmail.com>
2018-02-26 10:10:15 +0900

    '비밀번호찾기' 메일 형식

---
+++ application/views/common/mailTemplate.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta name="author" content="행복학교" />
+    <meta name="description" content="법륜스님과 함께하는 행복학교">
+    <title>비밀번호발송</title>
+    <style>
+        body{position: relative;padding: 0;margin: 0;}
+        .clearfix{*zoom:1}
+        .clearfix:after{content:'';display:block;clear:both;}
+        .mailWrap{width: 100%;background: #fff;}
+        .mailTop{background-color: #00c49f;height: 70px;}
+        .mailCon{padding: 20px 15%;}
+        .mailBottom{padding: 20px 15%; background-color: #eee;}
+        .mailBottom .tit{color: #959595;font-weight: bold;font-size: 20px;text-align: right}
+        .mailBottom .txt{color: #959595;text-align: right}
+        .mailBottom .left{float: left; width: 30%;}
+        .mailBottom .left.logo{padding-top:15px;}
+        .mailBottom .left .info{padding-top: 20px;padding-left: 10px;}
+        .mailBottom .left .info p{font-size: 12px;margin: 0px;line-height: 1.5}
+        .mailBottom .right{float: right;width: 70%;}
+    </style>
+</head>
+<body>
+    <div class="mailWrap">
+        <!--topbar-->
+        <div class="mailTop"></div><!--//topbar-->
+        <!--mail contents-->
+        <div class="mailInner">
+            <div class="mailCon">
+                <p><strong><span class="name"></span>님 안녕하세요,</strong> 행복학교입니다.</p>
+                <p>회원님께서 '비밀번호 찾기'를 통해 요청하신 임시비밀번호를 보내드립니다.</p>
+                <br>
+                <p><strong>임시비밀번호</strong>: <span class="tempPwd"></span></p>
+                <br>    
+                <p>임시비밀번호로 로그인 하신 후 꼭 비밀번호를 변경해주세요.</p>
+                <p>홈페이지 상단 마이페이지에서 비밀번호를 변경할 수 있습니다.</p>
+                <p>감사합니다.</p>
+                <br>
+                <p>홈페이지로 이동하려면 아래의 링크를 방문하시기 바랍니다.<br>
+                <a href="http://hihappy.eduniety.cc/">http://hihappy.eduniety.cc</a></p>
+                <p>*본 메일은 발송 전용 메일입니다.</p>
+            </div>
+        </div><!--//mail contents-->
+    </div>
+    <!--footer-->
+    <div class="mailBottom clearfix">
+        <div class="left logo">
+            <a href="http://hihappy.eduniety.cc/"><img src="http://hihappy.eduniety.cc/images/logo-gray.png" alt="행복학교"></a>
+            <div class="info">
+                <p>서울 서초구 서초동 1623-2 우일빌딩 3층</p>
+                <p>P: (02) 567-8080</p>
+            </div>    
+        </div>
+        <div class="right">
+            <p class="tit">"행복도 배울 수 있나요?"</p>
+            <p class="txt">오늘의 행복을 내일로 미루지 않는 법.<br>행복학교에서 오늘 내 삶에 만족하고 감사하며 지금 이대로 행복해지는 법을 만나보세요.</p>
+        </div>
+    </div>
+    <!--//footer-->
+</body>
+</html>
