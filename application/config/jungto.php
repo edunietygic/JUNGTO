c7bdc703a701efb740d33284f06c84d7d67c1704
jhseo <sseog818@gmail.com>
2018-02-28 15:56:59 +0900

    config 파일 변경

---
+++ application/config/jungto.php
@@ -0,0 +1,7 @@
+<?php
+// defined('BASEPATH') OR exit('No direct script access allowed');
+$config['bbs'] = array(
+	 'camp' => array('tabseq'=>'100','title'=>'행복캠프')
+	,'lecture' => array('tabseq'=>'200','title'=>'행복한강연')
+	,'review' => array('tabseq'=>'300','title'=>'생생후기')
+);
