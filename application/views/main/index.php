07c79def360592008d7f2e1b6e5a89c258d14a08
ziyunn <jykim306@gmail.com>
2018-03-06 15:44:58 +0900

    메인페이지 백그라운드 영상 수정 및 스타일 추가 * 동일한 이름으로 덮은 이미지 확인해볼 것

--- application/views/main/index.php
+++ application/views/main/index.php
@@ -1,11 +1,11 @@
 <!-- SECTION IMAGE FULLSCREEN --> 
-  <section class="fullscreen" data-vide-bg="<?=SKINURL?>/homepages/video-background/video/Relaxation" data-video-options="position: 0% 50%"> 
+  <section class="fullscreen" data-vide-bg="<?=SKINURL?>/homepages/video-background/video/Leaves_of_Tree" data-video-options="position: 0% 50%"> 
     <div class="container-fluid">
       <div class="container-fullscreen">
         <div class="text-middle text-center"><i class="fa fa-bullseye"></i>
           <h3 class="m-b-0"></h3>
           <h2 class="text-medium m-t-0"><img src="<?=SKINURL?>/images/happy-is.png" width="100%" alt="head"></h2>
-          <p class="lead hidden-xs">행복학교에서 오늘 내 삶에 만족하고 감사하며 <br>
+          <p class="lead hidden-xs" style="color: #000000">행복학교에서 오늘 내 삶에 만족하고 감사하며 <br>
             지금 이대로 행복해지는 법을 만나보세요.</p>
           <a class="btn btn-dark btn-outline" href="<?=HOSTURL?>/course">오늘의 행복을 내일로 미루지 않는 법</a> </div>
       </div>
