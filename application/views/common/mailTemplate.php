ec63751c91891ffb0e7c8ffc7d2bb8cdb6e34c3b
ziyunn <jykim306@gmail.com>
2018-03-06 15:29:06 +0900

    메일링 스타일 수정

--- application/views/common/mailTemplate.php
+++ application/views/common/mailTemplate.php
@@ -7,40 +7,40 @@
     <meta name="description" content="법륜스님과 함께하는 행복학교">
     <title>비밀번호발송</title>
 </head>
-<body style="position: relative;padding: 0;margin: 0;background: #eee;">
-    <div class="mailWrap" style="width: 100%;margin-top: 30px;">
+<body style="position: relative;padding-bottom: 120px;margin: 0;background: #eee;">
+    <div style="width: 100%;padding-top: 20px;">
         <!--topbar-->
-        <div class="mailTop" style="background-color: #00c49f;height: 20px;margin: 0 20%;"></div><!--//topbar-->
+        <div style="background-color: #00c49f;height: 15px;margin: 0 20%;"></div><!--//topbar-->
         <!--mail contents-->
-        <div class="mailInner">
-            <div class="mailCon" style="padding: 20px 5%;margin: 0 20%;background: #fff;">
-                <p style="font-size: 18px;"><strong><span class="name"><?=$name?></span>님 안녕하세요,</strong> 행복학교입니다.</p>
-                <p>회원님께서 '비밀번호 찾기'를 통해 요청하신 임시비밀번호를 보내드립니다.</p>
+        <div>
+            <div style="padding: 20px 5%;margin: 0 20%;background: #fff;">
+                <p style="font-size: 15px;"><span style="color: #00c49f" class="name"><?=$name?></span>님 안녕하세요, 행복학교입니다.</p>
+                <p style="font-size: 15px;">회원님께서 '비밀번호 찾기'를 통해 요청하신 임시비밀번호를 보내드립니다.</p>
                 <br>
-                <p><strong>임시비밀번호</strong>: <span class="tempPwd"><?=$tmp_password?></span></strong></p>
+                <p style="font-size: 15px;">임시비밀번호: <span class="tempPwd" style="color: #00c49f"><?=$tmp_password?></span></p>
                 <br>
-                <p style="font-size: 18px;">임시비밀번호로 로그인 하신 후 꼭 비밀번호를 변경해주세요.</p>
-                <p style="font-size: 18px;">홈페이지 상단 마이페이지에서 비밀번호를 변경할 수 있습니다.</p>
-                <p style="font-size: 18px;">감사합니다.</p>
+                <p style="font-size: 15px;">임시비밀번호로 로그인 하신 후 꼭 비밀번호를 변경해주세요.</p>
+                <p style="font-size: 15px;">홈페이지 상단 마이페이지에서 비밀번호를 변경할 수 있습니다.</p>
+                <p style="font-size: 15px;">감사합니다.</p>
                 <br>
-                <p style="font-size: 18px;">홈페이지로 이동하려면 아래의 링크를 방문하시기 바랍니다.<br>
-                <a href="http://jungto.eduniety.cc:8090/main">http://jungto.eduniety.cc:8090/main</a></p>
-                <p style="font-size: 18px;">*본 메일은 발송 전용 메일입니다.</p>
+                <p style="font-size: 15px;">홈페이지로 이동하려면 아래의 링크를 방문하시기 바랍니다.<br>
+                <a href="http://jungto.eduniety.cc:8090/main" style="font-size: 15px; text-decoration: underline; color: #00c49f">http://jungto.eduniety.cc:8090/main</a></p>
+                <p style="font-size: 13px; padding-top:20px;">*본 메일은 발송 전용 메일입니다.</p>
             </div>
         </div><!--//mail contents-->
     </div>
     <!--footer-->
-    <div class="mailBottom clearfix" style="*zoom: 1;padding: 20px 5%;margin: 0 20%;background: #fff;border-top: 2px dotted #959595;">
-        <div class="left logo" style="float: left;width: 40%;padding-top: 15px;">
+    <div class="mailBottom clearfix" style="*zoom: 1;padding: 30px 5% 110px;margin: 0 20%;background: #fff;border-top: 1px dotted #959595;">
+        <div class="left logo" style="float: left;width: 40%;">
             <a href="http://jungto.eduniety.cc:8090/main"><img src="<?=SKINURL?>/images/logo-gray.png" alt="행복학교"></a>
-            <div class="info" style="padding-top: 20px;padding-left: 10px;">
-                <p style="font-size: 12px;margin: 0px;line-height: 1.5;">서울 서초구 서초동 1623-2 우일빌딩 3층</p>
-                <p style="font-size: 12px;margin: 0px;line-height: 1.5;">P: (02) 567-8080</p>
+            <div class="info">
+                <p style="font-size: 10px;margin: 0px;line-height: 1.5;">서울 서초구 서초동 1623-2 우일빌딩 3층</p>
+                <p style="font-size: 10px;margin: 0px;line-height: 1.5;">P: (02) 567-8080</p>
             </div>
         </div>
         <div class="right" style="float: right;width: 60%;">
-            <p class="tit" style="font-size: 18px;color: #959595;font-weight: bold;text-align: right;">"행복도 배울 수 있나요?"</p>
-            <p class="txt" style="font-size: 14px;color: #959595;text-align: right;">오늘의 행복을 내일로 미루지 않는 법.<br>행복학교에서 오늘 내 삶에 만족하고 감사하며 지금 이대로 행복해지는 법을 만나보세요.</p>
+            <p class="tit" style="font-size: 15px;color: #959595;font-weight: bold;text-align: right;">"행복도 배울 수 있나요?"</p>
+            <p class="txt" style="font-size: 10px;color: #959595;text-align: right;">오늘의 행복을 내일로 미루지 않는 법.<br>행복학교에서 오늘 내 삶에 만족하고 감사하며 지금 이대로 행복해지는 법<br>을 만나보세요.</p>
         </div>
     </div>
     <!--//footer-->
