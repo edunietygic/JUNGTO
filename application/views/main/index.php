46febc57ae77af21d8a89e3e82f28e93147033f7
ziyunn <jykim306@gmail.com>
2018-04-25 10:39:28 +0900

    이미지 비율 수정

--- application/views/main/index.php
+++ application/views/main/index.php
@@ -4,7 +4,9 @@
       <div class="container-fullscreen">
         <div class="text-middle text-center"><i class="fa fa-bullseye"></i>
           <h3 class="m-b-0"></h3>
-          <h2 class="text-medium m-t-0"><img style="width: 70%" src="<?=SKINURL?>/images/happy-is.png" width="100%" alt="head"></h2>
+          <h2 class="text-medium m-t-0 hidden-xs">
+            <img src="<?=SKINURL?>/images/happy-is.png" alt="head">
+          </h2>
           <!-- <p class="lead hidden-xs" style="color: #000000">행복학교에서 오늘 내 삶에 만족하고 감사하며 <br>
             지금 이대로 행복해지는 법을 만나보세요.</p> -->
           <a class="btn btn-dark btn-outline" style="font-size: 16px; padding: 16px 32px;" href="<?=HOSTURL?>/course">행복학교 신청하기</a> </div>
